9979cf5313da0c77c03669a421b85fd760aa10bb
JudgeJenny <33179961+JudgeJenny@users.noreply.github.com>
2026-07-11 12:38:42 -0400

    Update main2.py

--- Temperature converter/main2.py
+++ Temperature converter/main2.py
@@ -11,8 +11,8 @@
 def converter(temp, direction): # Temperature converter and statements
     """ This function takes the user input for temperature and conversion direction, performs the math
     and generates the statement. """
-    temp = float(temp)
-    direction = int(direction)
+    temp = float(temp) 
+    direction = int(direction) 
     if direction == 1: # celsius to farenheit
         farenheit = temp * 9 / 5 + 32
         print(f"{temp} degrees Celsius is {farenheit} degrees Farenheit")
@@ -37,30 +37,31 @@ keepgoing = True
 while keepgoing == True:
 
     # Get the input from the user of which conversion they would like to perform
-    direction = input("Would you like to convert from: \n 1. Celsius to Farenheit \n 2. Farenheit to Celsius \nPlease enter 1 or 2: ")
+    direction = input("Would you like to convert from: \n 1. Celsius to Farenheit \n 2. Farenheit to Celsius \nPlease enter 1 or 2: ").strip()
 
     # Confirm that valid input was received - i.e. either 1 or 2. If not prompt user to enter again.
     while isnumber(direction) == False or float(direction) not in (1,2):
-        direction = input("That's not a valid input. Please enter 1 or 2: ")
+        direction = input("That's not a valid input. Please enter 1 or 2: ").strip()
 
     # Get user input on the temperature they would like to convert. This must be a valid number.
-    temp = input("What temperature would you like to convert? ")
+    temp = input("What temperature would you like to convert? ").strip()
 
     # Confirm that valid input was received. If not, prompt user to enter again.
     while isnumber(temp) == False:
-        temp = input("That's not a valid temperature. Please enter a number: ")
+        temp = input("That's not a valid temperature. Please enter a number: ").strip()
 
     converter(temp, direction) # Run the converter function to perform the math and print the statement
 
     # Prompt user if they would like to convert again
-    again = input("Would you like to convert again? Y/N: ")
+    again = input("Would you like to convert again? Y/N: ").strip().lower()
 
-    # 
-    while again not in ("Y", "N"):
-        again = input("That's not a valid input. Please enter Y or N: ")
+    # Check that valid input was received. If not, prompt user again.
+    while again not in ("y", "n"):
+        again = input("That's not a valid input. Please enter Y or N: ").strip().lower()
     
-    if again == "Y":
+    # Turn off the converter if user does not want to convert again
+    if again == "y":
         keepgoing = True
-    elif again == "N":
+    elif again == "n":
         keepgoing = False
         print("Thanks for converting with us today!")
